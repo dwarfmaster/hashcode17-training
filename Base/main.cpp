3fcffbf34bdf6b13ad4340e56129f89b86fdd2e6
Thibaut Perami <perami@clipper.ens.fr>
2017-02-18 14:41:50 +0100

    PizzaData

---
+++ Base/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long int llu;
+
+struct PizzaData{
+    lli toma;
+    lli mush;
+};
+
+
+
+int main ()
+{
+    
+}
